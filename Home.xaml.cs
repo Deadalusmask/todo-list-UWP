c990c90878bce8a62ef5c655f743bbf7fbe8dcc9
Deadalus <Deadalusmask@gmail.com>
2016-03-02 08:14:10 +0800

    -

---
+++ Home.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上提供
+
+namespace todo_list
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class Home : Page
+    {
+        public Home()
+        {
+            this.InitializeComponent();
+        }
+
+
+        private void maincalender_SelectedDatesChanged(CalendarView sender, CalendarViewSelectedDatesChangedEventArgs args)
+        {
+            var selectedDates = sender.SelectedDates.Select(p =>p.Date.Year.ToString()+"/"+ p.Date.Month.ToString() + "/" + p.Date.Day.ToString()).ToArray();
+            var values = string.Join(", ", selectedDates);
+            ThisDay.Text = values;
+            MyFlyout.ShowAt(FlyOutPlace);
+        }
+
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            MyFlyout.Hide();
+        }
+
+        private void ViewTodayButton_Click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(ViewEvent));
+        }
+
+        private void AddNewEventButton_Click(object sender, RoutedEventArgs e)
+        {
+            var parameters = new TextBlock();
+            parameters.Text = ThisDay.Text;
+            Frame.Navigate(typeof(AddPage),parameters);
+        }
+
+
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+//“空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 上有介绍
+
+namespace todo_list
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Enabled;
+            ApplicationView.PreferredLaunchViewSize = new Size(680,450);
+            ApplicationView.PreferredLaunchWindowingMode = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size { Width = 680, Height = 450 });
+        
+
+            BackButton.Visibility = Visibility.Collapsed;
+            HomeListBoxItem.IsSelected = true;
+            MainContent.Navigate(typeof(Home));
+
+        }
+
+        private void HamburgerButton_Click(object sender, RoutedEventArgs e)
+        {
+            MySplitView.IsPaneOpen = !MySplitView.IsPaneOpen;
+        }
+
+        private void IconsListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (HomeListBoxItem.IsSelected) {
+                MainContent.Navigate(typeof(Home));
+                BackButton.Visibility = Visibility.Collapsed;
+            }
+            else if (EventListBoxItem.IsSelected)
+            {
+                MainContent.Navigate(typeof(EventList));
+                BackButton.Visibility = Visibility.Visible;
+            }
+            else if (SettingsListBoxItem.IsSelected) {
+                MainContent.Navigate(typeof(Settings));
+                BackButton.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if(MainContent.CanGoBack)
+            {
+                MainContent.GoBack();
+                HomeListBoxItem.IsSelected = true;
+            }
+        }
+
+    }
+}
